e42ffca08dda85f592e793923fc25f01fdb8fdd9
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2021-11-19 10:54:29 +0100

    moving css

--- src/components/home/HomeMessage.vue
+++ src/components/home/HomeMessage.vue
@@ -9,31 +9,3 @@
         <button>Start Game</button>
     </div>
 </template>
-
-<style scoped>
-.welcome{
-    position: absolute;
-    top: 80%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 60px 40px;
-    height: 300px;
-    background-color: rgba(255, 255, 255, 0.4);
-    backdrop-filter: blur(16px);
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: space-between;
-    border-radius: 6px;
-}
-
-
-p, button{
-    font-size: 20px;
-}
-
-button{
-    padding: 20px;
-    width: 200px;
-    margin: 0 auto;
-}
-</style>
