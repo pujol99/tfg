ddd4b0ae11f9546554e9dd9bd7f5ec5eb281bef9
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2021-12-11 11:20:02 +0100

    welcome page template

--- src/components/scenes/welcome/Message.vue
+++ src/components/scenes/welcome/Message.vue
@@ -1,5 +1,15 @@
 <template>
-    Hello
+    <div class="welcome">
+        <div class="welcome-title">
+            <h1>Hello world</h1>
+        </div>
+        <div class="welcome-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, odit.</p>
+        </div>
+        <div class="welcome-action">
+            <button @click="this.$store.commit('nextScene')">Continue</button>
+        </div>
+    </div>
 </template>
 <script>
 export default {
