31aa9c0a5461d5a31381129a77d603b8ce0081d1
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2021-12-18 10:47:11 +0100

    undo last commit

--- tests/unit/stages.spec.js
+++ tests/unit/stages.spec.js
@@ -13,3 +13,14 @@ test("Get to second screen", async () => {
     
     expect(wrapper.html()).toContain("About you");
 });
+
+test("Popup activates in Quiz (no selected options)", async () => {
+    const wrapper = mount(Quiz, {
+        global: {
+            plugins: [store],
+        },
+    });
+    await wrapper.find("button").trigger("click");
+
+    expect(wrapper.html()).toContain("popupActive");
+});
