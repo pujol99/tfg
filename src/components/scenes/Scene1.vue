d89f94c3b08027e16b59eb5be91bae43408d3bf3
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2022-01-05 17:18:04 +0100

    camera animation done

--- src/components/scenes/Scene1.vue
+++ src/components/scenes/Scene1.vue
@@ -1,6 +1,10 @@
 <template>
-    <Renderer ref="renderer" resize="window" orbitCtrl>
-        <PerspectiveCamera ref="camera" :position="cameraPositions[camera]" />
+    <Renderer ref="renderer" resize="window">
+        <PerspectiveCamera
+            ref="camera"
+            :position="cameraPosition"
+            :lookAt="cameraLookAt"
+        />
         <Scene ref="scene" background="#000000">
             <AmbientLight :intensity="0.2" />
             <PointLight :intensity="0.5" :position="{ x: 0, y: 3, z: 0 }" />
@@ -25,24 +29,28 @@
         </Scene>
     </Renderer>
 </template>
+s
 
 <script>
 import { mapGetters } from "vuex";
-import Loader from "../utils/Loader.vue";
+import { gsap } from "gsap";
 export default {
-    components: {
-        Loader,
-    },
     data() {
+        let cameraLookAts = [
+            { x: 0, y: 0, z: 1 },
+            { x: 1, y: 3, z: 1 },
+        ];
+        let cameraPositions = [
+            { x: 3, y: 3, z: 1 },
+            { x: 5, y: 5, z: 1 },
+        ];
         return {
-            cameraPositions: [
-                { x: 10, y: 10, z: 0 },
-                { x: 1, y: 1, z: 0 }
-            ]
-        }
-    },
-    computed: {
-        ...mapGetters({ camera: "stages/getSceneCamera" }),
+            cameraPositions,
+            cameraPosition: Object.assign({}, cameraPositions[0]),
+            cameraLookAts,
+            cameraLookAt: Object.assign({}, cameraLookAts[0]),
+            ANIM_TIME: 1.0,
+        };
     },
     mounted() {
         this.scene = this.$refs.scene;
@@ -53,6 +61,25 @@ export default {
         this.renderer.onBeforeRender(() => {
             this.$refs.loader.update();
         });
+    },
+    computed: {
+        ...mapGetters({ camera: "stages/getSceneCamera" }),
+    },
+    watch: {
+        camera(newValue, oldValue) {
+            this.swap(this.cameraPosition, this.cameraPositions[newValue]);
+            this.swap(this.cameraLookAt, this.cameraLookAts[newValue]);
+        },
+    },
+    methods: {
+        swap(from, to) {
+            gsap.to(from, {
+                duration: this.ANIM_TIME,
+                x: to.x,
+                y: to.y,
+                z: to.z,
+            });
+        },
     },
 };
 </script>
