103dbe9e5cf404d0de01b2477f9860b62724246a
pujol99 <alexpujolcomet8@gmail.com>
2022-03-14 18:45:11 +0100

    build

--- docs/assets/shaders/screen.js
+++ docs/assets/shaders/screen.js
@@ -19,13 +19,13 @@ export var fs = `
 
     uniform float uTime;
     uniform sampler2D uTexture;
+    uniform vec2 uRes;
 
     vec2 curvature = vec2(7.);
-    vec2 screenResolution = vec2(200.);
-    vec2 scanLineOpacity = vec2(0.1);
+    float scanLineOpacity = 0.1;
     float vignetteOpacity = 0.5;
     float brightness = 1.;
-    float vignetteRoundness = 1.;
+    float vignetteRoundness = 1.0;
 
     vec2 curveRemapUV(vec2 uv) {
         uv = uv * 2.0 - 1.0;
@@ -36,7 +36,7 @@ export var fs = `
     }
 
     vec4 scanLineIntensity(float uv, float resolution, float opacity) {
-        float intensity = sin(uv * resolution * PI * 2.0 + uTime * 9.);
+        float intensity = sin(uv * resolution * PI * 2.0);
         intensity = ((0.5 * intensity) + 0.5) * 0.9 + 0.1;
         return vec4(vec3(pow(intensity, opacity)), 1.0);
     }
@@ -45,18 +45,21 @@ export var fs = `
         float intensity = uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y);
         return vec4(vec3(clamp(pow((resolution.x / roundness) * intensity, opacity), 0.0, 1.0)), 1.0);
     }
-
+    
     void main(void)
     {
         vec2 remappedUV = curveRemapUV(vUv);
         vec4 baseColor = texture2D(uTexture, remappedUV);
 
-        baseColor *= vignetteIntensity(remappedUV, screenResolution, vignetteOpacity, vignetteRoundness);
+        baseColor *= vignetteIntensity(remappedUV, uRes, vignetteOpacity, vignetteRoundness);
 
-        baseColor *= scanLineIntensity(remappedUV.x, screenResolution.y, scanLineOpacity.x);
-        baseColor *= scanLineIntensity(remappedUV.y, screenResolution.x, scanLineOpacity.y);
+        float lineOpacity = scanLineOpacity;
 
-        baseColor *= vec4(vec3(brightness), 1.0);
+        baseColor *= scanLineIntensity(remappedUV.x, uRes.y, lineOpacity);
+        baseColor *= scanLineIntensity(remappedUV.y, uRes.x, lineOpacity);
+
+        float strength = 1.2 - 0.6*distance(vUv.y, sin(uTime*1.5)*0.5+0.5);
+        baseColor *= vec4(vec3(brightness*strength*strength), 1.0);
 
         if (remappedUV.x < 0.0 || remappedUV.y < 0.0 || remappedUV.x > 1.0 || remappedUV.y > 1.0){
             gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
